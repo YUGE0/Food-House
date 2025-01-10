8995618cf0a406e29b6cfe6fcfe5d06884953fca
YUGE0 <yugprajapati32@gmail.com>
2025-01-10 14:32:52 +0530

    Booking form: build error solve 1

--- app/book/page.tsx
+++ app/book/page.tsx
@@ -6,8 +6,8 @@ export default function Page() {
   return (
     <div className="my-10 md:my-32">
       <h1 className="text-center">Reserve Your Moment, Relish Every Bite.</h1>
-      <p className="text-center p-5">"Make every meal a memory to treasure. Reserve your spot at Food House and savor dishes crafted with care, passion, and the finest ingredients. 
-        From the first bite to the last, let us make your dining experience unforgettable."</p>
+      <p className="text-center p-5">Make every meal a memory to treasure. Reserve your spot at Food House and savor dishes crafted with care, passion, and the finest ingredients. 
+        From the first bite to the last, let us make your dining experience unforgettable.</p>
       <Tablebook/>
       <div className="p-10 flex flex-col gap-10 lg:flex-row justify-around">
         <div className="bg-black text-white lg:w-[30%] justify-items-center p-10 rounded-3xl">
