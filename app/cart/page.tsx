20819526385253a31c4cf161faf812c926d4e953
YUGE0 <yugprajapati32@gmail.com>
2025-01-21 14:10:18 +0530

    Cart additional info added

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,16 +1,20 @@
+"use client"
 import React from 'react'
 import menu,{Dish} from '../ApiDemo/MenuD';
 import CCard from '../compo/CCard';
 import Image from 'next/image';
 
 export default function Page() {
+  const bdate = new Date()
   return (
-    <div className="p-4 m-5 md:mx-44 rounded-3xl border border-black/20 shadow-md shadow-black/50">
-        <div className="flex justify-around my-2">
-            <Image src="/FoodHouse Logo.svg" width={300} height={100} alt="Food House Logo"/>
-        </div>
+    <div className="p-4 m-5 md:mx-44 rounded-3xl border border-black/20 shadow-sm shadow-black/50">
+      <div className="flex justify-around my-2">
+          <Image src="/FoodHouse Logo.svg" width={300} height={100} alt="Food House Logo"/>
+      </div>
       <h1 className="text-5xl rounded-sm border-black/80 border-x-2 text-center my-2 uppercase">Your Order</h1>
+      <div className="flex justify-between"><h3>Name</h3><h3>date</h3></div>
       {menu.map((category) => (<CategorySection key={category.category} category={category.category} dishes={category.dishes}/>))}
+      <button className="bg-black text-white p-2 w-full rounded-2xl shadow-md shadow-fcolor/60"><h1>Place Order</h1></button>
     </div>
   );
 }
--- app/compo/CCard.tsx
+++ app/compo/CCard.tsx
@@ -16,12 +16,12 @@ export default function CCard(fc: {
     const decrement = () => {
     setQuantity((prevQuantity) => Math.max(0, prevQuantity - 1));};
   return (
-    <div className="p-2 bg-black text-white flex rounded-xlp border border-black/20 shadow-md shadow-black/20 sm:w-[42%] md:w-[80%] lg:w-[40%] xl:w-[30%] 2xl:w-[98%]">
+    <div className="p-2 bg-black text-white flex rounded-xlp border border-black/20 shadow-md shadow-black/80 sm:w-[42%] md:w-[80%] lg:w-[40%] xl:w-[30%] 2xl:w-[98%]">
         <button className="p-1 justify-self-end"><Image src={"/CloseMenuWhite.svg"}  width={80} height={0} alt={fc.name}/></button>
         <Image className="rounded-xl w-[32%] h-full" src={`/${fc.img}`}  width={300} height={0} alt={fc.name}/>
         <div className="w-full flex flex-col justify-between ml-2">
         <h3 className='h-16'>{fc.name}</h3>
-        <div className="mt-2 w-full flex justify-between items-end">
+        <div className="mt-6 w-full flex justify-between items-end">
             <h3 className="font-semibold pr-3">{fc.price}₹</h3>
             <div className="flex w-full">
             <button onClick={decrement} className=" bg-white text-black p-1 px-2 rounded-full text-2xl font-bold font-work">-</button>
