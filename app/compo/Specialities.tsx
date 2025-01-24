4deb10ad998d3b468516a5a7a385601730c3f339
YUGE0 <yugprajapati32@gmail.com>
2025-01-24 14:30:09 +0530

    Specialities component changes.

--- app/compo/Specialities.tsx
+++ app/compo/Specialities.tsx
@@ -13,45 +13,45 @@ export default function Specialities() {
             <div className="py-10 flex flex-wrap justify-between lg:flex-col md:w-[74%] lg:w-[20%] gap-5">
                 <button onClick={()=>{setSpeciality("Appetizers")}}>
                     {currentSpeciality=="Appetizers"?
-                    <div className="p-1 pr-2 rounded-xlp border bg-bc border-bc/20 shadow-black/50 shadow-md flex gap-2 place-items-center">
+                    <div className="p-1 pr-2 rounded-xlp border bg-fcolor border-bc/20 shadow-black/50 shadow-md flex gap-2 place-items-center">
                         <Image className="p-2 w-12 rounded-xl" src="/appetizer.svg" width={50} height={50} alt="Food House Logo"/>
                         <h4 className="text-center text-white w-[76%]">Appetizers</h4>
                     </div>
                     :<div className="p-1 pr-2 rounded-xlp border border-black/20 shadow-black/50 shadow-md flex gap-2 place-items-center">
-                        <Image className="p-2 w-12 bg-blue-800 rounded-xl" src="/appetizer.svg" width={50} height={50} alt="Food House Logo"/>
+                        <Image className="p-2 w-12 bg-fcolor rounded-xl" src="/appetizer.svg" width={50} height={50} alt="Food House Logo"/>
                         <h4 className="text-center w-[76%]">Appetizers</h4>
                     </div>}
                 </button>
                 <button onClick={()=>{setSpeciality("Food")}}>
                     {currentSpeciality=="Food"?
-                    <div className="p-1 pr-2 rounded-xlp border bg-bc border-bc/20 shadow-black/50 shadow-md flex gap-2 place-items-center">
+                    <div className="p-1 pr-2 rounded-xlp border bg-fcolor border-bc/20 shadow-black/50 shadow-md flex gap-2 place-items-center">
                         <Image className="p-2 w-12 rounded-xl" src="/food.svg" width={50} height={50} alt="Food House Logo"/>
                         <h4 className="text-center text-white w-[76%]">Food</h4>
                     </div>
                     :<div className="p-1 pr-2 rounded-xlp border border-black/20 shadow-black/50 shadow-md flex gap-2 place-items-center">
-                        <Image className="p-2 w-12 bg-blue-800 rounded-xl" src="/food.svg" width={50} height={50} alt="Food House Logo"/>
+                        <Image className="p-2 w-12 bg-fcolor rounded-xl" src="/food.svg" width={50} height={50} alt="Food House Logo"/>
                         <h4 className="text-center w-[76%]">Food</h4>
                     </div>}
                 </button>
                 <button onClick={()=>{setSpeciality("Rice")}}>
                     {currentSpeciality=="Rice"?
-                    <div className="p-1 pr-2 rounded-xlp border bg-bc border-bc/20 shadow-black/50 shadow-md flex gap-2 place-items-center">
+                    <div className="p-1 pr-2 rounded-xlp border bg-fcolor border-bc/20 shadow-black/50 shadow-md flex gap-2 place-items-center">
                         <Image className="p-2 w-12 rounded-xl" src="/rice.svg" width={50} height={50} alt="Food House Logo"/>
                         <h4 className="text-center text-white w-[76%]">Rice</h4>
                     </div>
                     :<div className="p-1 pr-2 rounded-xlp border border-black/20 shadow-black/50 shadow-md flex gap-2 place-items-center">
-                        <Image className="p-2 w-12 bg-blue-800 rounded-xl" src="/rice.svg" width={50} height={50} alt="Food House Logo"/>
+                        <Image className="p-2 w-12 bg-fcolor rounded-xl" src="/rice.svg" width={50} height={50} alt="Food House Logo"/>
                         <h4 className="text-center w-[76%]">Rice</h4>
                     </div>}
                 </button>
                 <button onClick={()=>{setSpeciality("Desserts")}}>
                     {currentSpeciality=="Desserts"?
-                    <div className="p-1 pr-2 rounded-xlp border bg-bc border-bc/20 shadow-black/50 shadow-md flex gap-2 place-items-center">
+                    <div className="p-1 pr-2 rounded-xlp border bg-fcolor border-bc/20 shadow-black/50 shadow-md flex gap-2 place-items-center">
                         <Image className="p-2 w-12 rounded-xl" src="/desserts.svg" width={50} height={50} alt="Food House Logo"/>
                         <h4 className="text-center text-white w-[76%]">Desserts</h4>
                     </div>
                     :<div className="p-1 pr-2 rounded-xlp border border-black/20 shadow-black/50 shadow-md flex gap-2 place-items-center">
-                        <Image className="p-2 w-12 bg-blue-800 rounded-xl" src="/desserts.svg" width={50} height={50} alt="Food House Logo"/>
+                        <Image className="p-2 w-12 bg-fcolor rounded-xl" src="/desserts.svg" width={50} height={50} alt="Food House Logo"/>
                         <h4 className="text-center w-[76%]">Desserts</h4>
                     </div>}
                 </button>
